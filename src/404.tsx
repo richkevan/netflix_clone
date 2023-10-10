baec5298a5459b86bca74bba3fdf62ea67604695
Rich Kevan <rskevan@me.com>
2023-10-09 20:17:55 -0700

    updates to 404 page.

--- src/404.tsx
+++ src/404.tsx
@@ -9,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <div className="h-screen w-full flex flex-col justify-center items-center">
       <h1>404</h1>
       <h2>Page Not Found</h2>
+      <div>You have reached a page that does not exist please use the back button to return to the previous page or return to my <a href="https://richkevan.com">PORTFOLIO</a></div>
       </div>
     </div>
   </React.StrictMode>,
